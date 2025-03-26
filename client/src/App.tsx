ff6119195f235926e50a5d160b89fee9e81d575f
winnie <winnwyz@gmail.com>
2025-03-26 12:40:33 +1100

    Set up frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="bg-orange-300 hover:bg-orange-500">hello</Button>
+    </>
+  );
+}
+
+export default App;

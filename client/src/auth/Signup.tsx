16c7c5b1c215b96e0a2eda6d12cfe9f78c736036
Winn Z <winnwyz@gmail.com>
2025-03-26 14:35:17 +1100

    Implement Signup page

--- client/src/auth/Signup.tsx
+++ client/src/auth/Signup.tsx
@@ -1,5 +1,142 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import {
+  Contact,
+  Loader2,
+  LockKeyhole,
+  Mail,
+  Phone,
+  PhoneOutgoing,
+  User,
+} from "lucide-react";
+import { ChangeEvent, useState } from "react";
+import { Link } from "react-router-dom";
+
+type SignupInputState = {
+  fullname: string;
+  email: string;
+  password: string;
+  contact: string;
+};
+
 const Signup = () => {
-  return <div>Signup</div>;
+  const [input, setInput] = useState<SignupInputState>({
+    fullname: "",
+    email: "",
+    password: "",
+    contact: "",
+  });
+
+  const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInput((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const LoginSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(input);
+  };
+
+  const loading = false;
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <form
+        onSubmit={LoginSubmitHandler}
+        className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4"
+      >
+        <div>
+          <h1 className="font-bold text-2xl">PatelEats</h1>
+        </div>
+
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="text"
+              placeholder="Full Name"
+              name="fullname"
+              value={input.fullname}
+              onChange={changeEventHandler}
+              className="pl-10 focus-visible:ring-1 border-gray-200"
+            />
+            <User className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+          </div>
+        </div>
+
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={input.email}
+              onChange={changeEventHandler}
+              className="pl-10 focus-visible:ring-1 border-gray-200"
+            />
+            <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+          </div>
+        </div>
+
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={input.password}
+              onChange={changeEventHandler}
+              className="pl-10 focus-visible:ring-1 border-gray-200"
+            />
+            <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+          </div>
+        </div>
+
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="text"
+              placeholder="Contact"
+              name="contact"
+              value={input.contact}
+              onChange={changeEventHandler}
+              className="pl-10 focus-visible:ring-1 border-gray-200"
+            />
+            <Phone className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+          </div>
+        </div>
+
+        <div className="mb-10">
+          {loading ? (
+            <Button
+              disabled
+              className="bg-orange-300 hover:bg-orange-400 w-full"
+            >
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please Wait
+            </Button>
+          ) : (
+            <Button
+              type="submit"
+              className="bg-orange-300 hover:bg-orange-400 w-full"
+            >
+              Sign Up
+            </Button>
+          )}
+        </div>
+        <Separator />
+        <p className="mt-2">
+          Already have an account?{" "}
+          <Link to="/login" className="text-blue-500">
+            Login
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
